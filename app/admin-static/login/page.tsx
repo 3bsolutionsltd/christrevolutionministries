2f5641fdc41273dc7da9ee02d9b1dc7f6006d0f5
Stephen Omwony <3bsolutionsltd@gmail.com>
2025-12-10 12:45:48 +0300

    Fix import path for static-content-manager in admin-static login

--- app/admin-static/login/page.tsx
+++ app/admin-static/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useStaticContentManager } from '../../lib/static-content-manager';
+import { useStaticContentManager } from '../../../lib/static-content-manager';
 
 export default function StaticAdminLogin() {
   const [token, setToken] = useState('');
